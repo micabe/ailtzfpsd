cc91251738710195f65b80e79f4bcf9a38dc0bdb
Гильмитдинов <nsgilmitdinov@sberbank.ru>
2021-02-23 23:39:21 +0300

    add tests

---
+++ tests/test_integration.py
@@ -0,0 +1,14 @@
+import json
+import requests
+import pytest
+
+def test_prometheus_is_available():
+    response = requests.get("http://localhost:9090")
+    assert response.ok
+
+def test_targets_connected_to_prometheus():
+    response = requests.get("http://localhost:9090/api/v1/targets")
+    data = json.loads(json.dumps(response.json()))
+    assert data["data"]["activeTargets"][0]["labels"]["instance"] == "node_exporter:9100"
+    assert data["data"]["activeTargets"][0]["health"] == "up"
+
